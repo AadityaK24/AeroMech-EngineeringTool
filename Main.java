298bc792ba917e80ecb457a694e1dd75a062a07f
AadityaK24 <aaditya2410a@gmail.com>
2026-09-26 17:06:06 +0530

    Fixed JavaFX imports and working simulator

--- Main.java
+++ Main.java
@@ -1,3 +1,7 @@
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
